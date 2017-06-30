90fe18a6f85528f3295197b6cbe9476cb5a7e22d
coyote91 <david.f.r91@hotmail.com>
2017-06-30 16:53:02 -0500

    añado menu hamburguesa de arriba hacia abajo

---
+++ modelo7/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+<link rel="stylesheet" href="./css/style.css">
+
+
+<script src="./js/app.js" charset="utf-8"></script>
+
+
+  </head>
+
+  <body>
+
+  <div id="myNav" class="overlay">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <div class="overlay-content">
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+    </div>
+  </div>
+
+  <h2>Fullscreen Overlay Nav Example</h2>
+  <p>Click on the element below to open the fullscreen overlay navigation menu.</p>
+  <p>In this example, the navigation menu will slide downwards from the top:</p>
+  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+
+
+
+  </body>
+</html>
---
+++ modelo7/js/app.js
@@ -0,0 +1,34 @@
+/**
+
+DE ARRIBA HACIA ABAJO
+
+*/
+
+
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+/*
+
+EJEJMPLO DE IZQUIERDA A DERECHA
+
+Open when someone clicks on the span element
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+*/
